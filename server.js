27d6ab83a56775f78c63e6899d93006a0258ee0b
Lingze Hu <lingzehu@gmail.com>
2020-04-27 20:53:33 -0400

    get started with mongoDB and basic CRUD on Bootcamp

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const express = require('express');
 const dotenv = require('dotenv');
 //const logger = require('./middleware/logger');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
-// Route files
-const bootcamps = require('./routes/bootcamps');
+
 
 // Load env vars
 dotenv.config({ path: './config/config.env'});
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if(process.env.NODE_ENV === 'development') {
@@ -26,6 +35,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+    );
+
+//Hanlde unhandled promis rejections
+process.on('unhandledRejection', (err, promis) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
