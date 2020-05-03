9c2da239d8e1cd4016487f7953e7ae832d87c8d9
Lingze Hu <lingzehu@gmail.com>
2020-05-02 22:53:04 -0400

    Course Schema and patch the seeder

--- seeder.js
+++ seeder.js
@@ -8,6 +8,8 @@ dotenv.config({path: './config/config.env'});
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
+
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -18,14 +20,19 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 // Read JSON files
-const bootcamp = JSON.parse(
+const bootcamps = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+const courses = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
 
 // Import into DB 
 const importData = async() => {
     try {
-        await Bootcamp.create(bootcamp);
+        await Bootcamp.create(bootcamps);
+        await Course.create(courses);
         console.log('Data Imported...'.green.inverse);
         process.exit();
     } catch(err) {
@@ -37,6 +44,7 @@ const importData = async() => {
 const deleteData = async() => {
     try {
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
         console.log('Data Destoryed...'.red.inverse);
         process.exit();
     } catch(err) {
