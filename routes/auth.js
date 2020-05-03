b4631ce7d7571098bee56ddb85d668cc29dedc51
Lingze Hu <lingzehu@gmail.com>
2020-05-03 17:14:07 -0400

    User Login

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { 
-    register
+    register,
+    login
 } = require('../controller/auth');
 
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
