3b9009e00475dbcd15abfc75a11ca14f8fde42b2
Lingze Hu <lingzehu@gmail.com>
2020-05-07 14:17:50 -0400

    API Security

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,17 +10,17 @@ exports.protect = asyncHandler(async (req, res, next) => {
     if(req.headers.authorization && 
         req.headers.authorization.startsWith('Bearer')
     ) {
-        
+        // set token from bearer token in header
         token = req.headers.authorization.split(' ')[1];
+    } 
+    // set token from cookie
+    else if(req.cookies.token) {
+        token = req.cookies.token;
     }
-
-    // else if(req.cookies.token) {
-    //     token = req.cookies.token;
-    // }
 
     // Make sure token exists
     if(!token) {
-        return next(new ErrorResponse('Not authorize to access this route', 401));
+        return next(new ErrorResponse('Not authorized to access this route', 401));
     }
 
     try {
@@ -33,7 +33,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
         next();
     } catch (err) {
-        return next(new ErrorResponse('Not authorize to access this route', 401));
+        return next(new ErrorResponse('Not authorized to access this route', 401));
     }
 });
 
